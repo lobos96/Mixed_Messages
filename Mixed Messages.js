c2b241eefa8f165647ca3622596f8897b94f7ecd
Aaron <aaron@Alfredos-MacBook-Air.local>
2022-01-05 13:23:54 -0800

    adds basic random message generating functionality

--- Mixed Messages.js
+++ Mixed Messages.js
@@ -1,13 +1,20 @@
-console.log("Wanna hear a joke? y/n")
+console.log("Wanna hear a joke?")
 
-const jokeGenerator = () => {
-    noun: ['monkey', 'robot', 'chicken', 'wiener dog', 'ex wife'];
-    verb: ['run to the store', 'jump on the bus', 'smoosh the sandwich', 'sneeze on the salad', 'squirt ketchup on the counter'];
-    punchline: ['To get to the other side!', "Orange you glad I didn't say banana!", 'To get to school on time!', 'To avoid a parking ticket!', 'To have room for seconds!'];
-    let randElem = obj => {
-        this.obj(Math.floor(math.random() * this.obj.length))
-    } 
-    return "Why did the " + randElem(noun) + randElem(verb) + randElem(punchline)
+const jokeGenerator = {
+    noun: ['monkey', 'robot', 'chicken', 'wiener dog', 'ex wife'],
+    verb: ['run to the store', 'jump on the bus', 'smoosh the sandwich', 'sneeze on the salad', 'squirt ketchup on the counter'],
+    punchline: ['To get to the other side!', "Because it's part of a complete breakfast!", 'To get to school on time!', 'To avoid a parking ticket!', 'To have room for seconds!'],
+    randElem(obj) {
+        return obj[Math.floor(Math.random() * obj.length)]
+    }, 
+    generateJoke() {return `Why did the ${this.randElem(this.noun)} ${this.randElem(this.verb)}? ${this.randElem(this.punchline)}`}
 }
 
-console.log(jokeGenerator())
+//var userAns = window.prompt("Type 'y' for yes or 'n' for no: ");
+
+//if (userAns !== 'y' && userAns !== 'n') {
+    //return "Not a valid input. Please type 'y' or 'n"
+//}
+//if (userAns === 'y') {
+    console.log(jokeGenerator.generateJoke())
+//}
