d0a787030d68aab747d9a4d4bf8fc193cebd53ae
Aaron <aaron@Alfredos-MacBook-Air.local>
2022-01-05 11:41:33 -0800

    adds Mixed Messages to development branch

--- Mixed Messages.js
+++ Mixed Messages.js
@@ -1,1 +1,13 @@
-console.log("Wanna hear a joke?")
+console.log("Wanna hear a joke? y/n")
+
+const jokeGenerator = () => {
+    noun: ['monkey', 'robot', 'chicken', 'wiener dog', 'ex wife'];
+    verb: ['run to the store', 'jump on the bus', 'smoosh the sandwich', 'sneeze on the salad', 'squirt ketchup on the counter'];
+    punchline: ['To get to the other side!', "Orange you glad I didn't say banana!", 'To get to school on time!', 'To avoid a parking ticket!', 'To have room for seconds!'];
+    let randElem = obj => {
+        this.obj(Math.floor(math.random() * this.obj.length))
+    } 
+    return "Why did the " + randElem(noun) + randElem(verb) + randElem(punchline)
+}
+
+console.log(jokeGenerator())
